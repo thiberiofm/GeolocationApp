0e344b333a2f4bc5ce15e82a3b5a5d566add9aa8
thiberiofm <thii.freitas@hotmail.com>
2019-11-03 23:06:54 -0300

    changing logic app and call to googleapi

--- app.py
+++ app.py
@@ -1,14 +1,12 @@
 import requests
-from flask import Flask, render_template, jsonify, make_response, request
-from flask_restful import abort
+from flask import Flask, jsonify, make_response, request
 
 from key import key
-import imghdr
 
 app = Flask(__name__)
 
 search_url = "https://maps.googleapis.com/maps/api/place/textsearch/json"
-photos_url = "https://maps.googleapis.com/maps/api/place/photo"
+details_url = "https://maps.googleapis.com/maps/api/place/details/json"
 
 
 @app.errorhandler(404)
@@ -17,44 +15,32 @@ def not_found(error):
     return make_response(jsonify({'error': 'Not Found'}), 404)
 
 
-@app.route("/", methods=["GET"])
-def retreive():
-    return render_template('layout.html')
-
-
 @app.route("/places/v1/places", methods=['GET'])
 def list_places():
     """
-       Function to fetch the places infos.
+       Function to receive the places infos to search at Googlemaps.
        """
 
     if not len(request.args) > 0:
-        return abort(400)
+        return make_response(jsonify({'error': 'Bad Request'}), 400)
 
-    return request.args
+    query_parameters = request.args
+    place_name = query_parameters.get('place_name')
 
+    query = place_name
 
-
-
-@app.route("/sendRequest/<string:query>")
-def results(query):
-    search_payload = {"key": key, "query": query}
+    search_payload = {"key":key, "query":query}
     search_req = requests.get(search_url, params=search_payload)
     search_json = search_req.json()
 
-    photo_id = search_json["results"][0]["photos"][0]["photo_reference"]
+    place_id = search_json["results"][0]["place_id"]
 
-    photo_payload = {"key": key, "maxwidth": 500, "maxwidth": 500, "photoreference": photo_id}
-    photo_request = requests.get(photos_url, params=photo_payload)
+    details_payload = {"key":key, "placeid":place_id}
+    details_resp = requests.get(details_url, params=details_payload)
+    details_json = details_resp.json()
 
-    photo_type = imghdr.what("", photo_request.content)
-    photo_name = "static/" + query + "." + photo_type
-
-    with open(photo_name, "wb") as photo:
-        photo.write(photo_request.content)
-
-    return '<img src=' + photo_name + '>'
-
+    url = details_json["result"]["url"]
+    return jsonify({'data':{ 'url' : url }})
 
 if __name__ == "__main__":
     app.run(debug=True)
